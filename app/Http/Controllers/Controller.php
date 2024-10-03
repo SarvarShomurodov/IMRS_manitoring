abd9b2b8550165a0d43197609b1ae275ccc3d5b7
SarvarShomurodov <sarvar9818sh@gmail.com>
2024-10-03 21:41:01 +0500

    Change

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,14 +13,39 @@ class Controller extends BaseController
     use AuthorizesRequests, ValidatesRequests;
     public function getBusinessTripCounts()
     {
-        $businessTripCounts = DB::table('quarters as q')
-        ->select('q.name', 
-            DB::raw('COUNT(bt.id) as number_of_business_trips'), 
-            DB::raw('(SELECT COUNT(*) FROM busines_trips WHERE quarters_id = q.id) AS total_business_trips'))
-            ->leftJoin('busines_trips as bt', 'q.id', '=', 'bt.quarters_id')
-            ->groupBy('q.id')
-            ->orderBy('number_of_business_trips', 'desc')
-            ->get();
+        $businessTripCounts = DB::select("
+        select
+    sum(s.first_quater) as first_quater,
+    sum(s.second_quater) as second_quater,
+    sum(s.third_quater) as third_quater,
+    sum(s.fourth_quater) as fourth_quater
+from (select count(q.id) first_quater, 0 second_quater, 0 third_quater, 0 fourth_quater
+               from busines_trips bt
+                        inner join quarters q on bt.quarters_id = q.id
+               where q.id = 1
+               group by bt.name
+               union all
+               select 0 first_quater, count(q.id) second_quater, 0 third_quater, 0 fourth_quater
+               from busines_trips bt
+                        inner join quarters q on bt.quarters_id = q.id
+               where q.id = 2
+               group by bt.name
+               union all
+               select  0 first_quater, 0 second_quater, count(q.id) third_quater, 0 fourth_quater
+               from busines_trips bt
+                        inner join quarters q on bt.quarters_id = q.id
+               where q.id = 3
+               group by bt.name
+               union all
+               select  0 first_quater, 0 second_quater, 0 third_quater, count(q.id) fourth_quater
+               from busines_trips bt
+                        inner join quarters q on bt.quarters_id = q.id
+               where q.id = 4
+               group by bt.name
+) s;
+    ");
+        // var_dump($businessTripCounts);
         return view('client.index',compact('businessTripCounts'));
+        // 
     }
 }
--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -3,5 +3,25 @@
 @section('title', 'Asosiy')
 
 @section('content')
+<table>
+  <tr>
+    <th>ID</th>
+    <th>Ko`rsatkich nomi</th>
+    <th>1-chorak</th>
+    <th>2-chorak</th>
+    <th>3-chorak</th>
+    <th>4-chorak</th>
+  </tr>
+ 
+  <tr>
+   <td>Sa</td>
+   <td></td>
+    <td>{{$businessTripCounts[0]->first_quater}}</td>
+    <td>{{$businessTripCounts[0]->second_quater}}</td>
+    <td>{{$businessTripCounts[0]->third_quater}}</td>
+    <td>{{$businessTripCounts[0]->fourth_quater}}</td>
+  </tr>
 
+
+</table> 
 @endsection
